4f19e6da4578fc5d0b6038649de8c8eee1e283ca
Ashoks-hub <79261622+Ashoks-hub@users.noreply.github.com>
2022-03-17 12:53:14 +0530

    Updated 5.5 and 4.8

--- tests/4_dobby_container_runtime_test.sh
+++ tests/4_dobby_container_runtime_test.sh
@@ -8,13 +8,12 @@ test_5_3() {
 	local DobbyInit_PID
 	
 	DobbyInit_PID=$(ps -fe | grep DobbyInit | grep $containername | awk '{print $2}')
+	
 	status=$(cat /proc/$DobbyInit_PID/status | grep CapPrm | awk '{print $2}')
 	output=$(capsh --decode=$status | sed 's/.*=//g')
-	
 	input=( cap_net_raw cap_dac_read_search cap_sys_module cap_sys_admin cap_sys_ptrace )
 	IFS=','
 	read -a ouputarr <<<"$output"
-	
 	#accessing each element of array
 	for i in "${!ouputarr[@]}";
 	do
@@ -36,6 +35,27 @@ test_5_3() {
 		done
 	done
 	pass "$check"
+}
+
+test_5_5() {
+        local testid="5.5"
+        local desc="Ensure sensitive host system directories are not mounted on containers"
+        local check="$testid - $desc"
+        local output
+        local Dobby_pid
+
+	Dobby_pid=$(ps -fe | grep DobbyInit | grep $containername | awk '{print $2}')
+        output_1=$(cat /proc/$Dobby_pid/mounts | grep "etc")
+        output_1=$(echo $output_1 | grep "rw")
+
+        output_2=$(cat /proc/$Dobby_pid/mounts | grep  -E 'bin|sbin|usr/bin|opt/logs')
+
+         if [ "$output_1" == "" -a "$output_2" == "" ]; then
+            pass "$check"
+            return
+        fi
+            fail "$check"
+	
 }
 
 test_5_9() {
@@ -60,8 +80,10 @@ test_5_10() {
 	local output
   
 	output=$(cat /sys/fs/cgroup/memory/$containername/memory.limit_in_bytes)
-   
-    if [ "$output" == "0" -o "$output" == "-1" ]; then
+   	
+	total=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+    
+	if [ "$output" == "0" -o "$output" == "-1" -o "$output" == "$total" ]; then
       fail "$check"
       return
     fi
@@ -75,13 +97,13 @@ test_5_12() {
         local output
         local Dobby_pid
 
-        Dobby_pid=$(pidof DobbyInit)
+        Dobby_pid=$(ps -fe | grep DobbyInit | grep $containername | awk '{print $2}')
         output=$(cat /proc/$Dobby_pid/mounts | grep "/ ")
         output_1=$(echo $output | awk '{ print $4}')
 
         if [ "$output_1" == "ro" ]; then
               pass "$check"
-               return
+                return
         fi
               fail "$check"
 
@@ -141,11 +163,11 @@ test_5_29() {
 	local desc="Ensure that Dobby's default bridge "dobby0" is not used"
 	local check="$testid - $desc"
 	local output
-	local ouput_1
+	local output_1
   
 	output=$(brctl show dobby0 | grep veth | awk '{ print $4}')
-	output_1=$(brctl show | grep dobby0 | awk '{ print $1}')
-   
+   	output_1=$(brctl show | grep dobby0 | awk '{ print $1}')
+    
     if [ "$output" == "veth0" -o "$output_1" == "dobby0" ]; then
       fail "$check"
       return
@@ -155,17 +177,19 @@ test_5_29() {
 
 test_5_31() {
 	local testid="5.31"
-	local desc="Ensure that the Dobby socket is not mounted inside any containers"
-	local check="$testid - $desc"
-	local output
-	local dobbypid
+        local desc="Ensure that the Dobby socket is not mounted inside any containers"
+        local check="$testid - $desc"
+        local output
+	local Dobby_pid
+
+	Dobby_pid=$(ps -fe | grep DobbyInit | grep $containername | awk '{print $2}')	
+	output=$(find /proc/$Dobby_pid/root/* -iname dobbyPty.sock | grep -v find)
 	
-	dobbypid=$(pidof DobbyInit)
-	output=$(find /proc/$dobbypid/root/* -iname dobbyPty.sock | grep -v find)
-   
-    if [ "$output" == "" ]; then
+	if [ "$output" == "" ]; then
       pass "$check"
       return
     fi
     fail "$check"
+
 }
+
